f64426b7ee92d45abf4829936a93b4d614d26a52
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-10-03 05:57:47 -0400

    done

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className='App'>
       <Header />
-      <div className='app_itemsContainer'>
+      <div className='app__itemsContainer'>
         <Item
           title='Lowest Cost Solar Panels in America'
           desc='Money-back guarantee'
