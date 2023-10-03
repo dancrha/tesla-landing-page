b0b192e6141427491ef7e564447811039c38b882
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-10-03 05:13:54 -0400

    on items section

---
+++ src/components/Item.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Button from "./Button";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const Item = ({
+  title,
+  desc,
+  descLink,
+  backgroundImg,
+  leftBtnTxt,
+  leftBtnLink,
+  rightBtnTxt,
+  rightBtnLink,
+  twoButtons,
+  first,
+}) => {
+  return (
+    <div className='item' style={{}}>
+      <div className='item__container'>
+        <div className='item__text'>
+          <p>{title}</p>
+          <div className='item_textDesc'>
+            <p>{desc}</p>
+          </div>
+        </div>
+        <div className='item_lowerThird'>
+          <div className='item_buttons'>
+            <Button imp='primary' text={leftBtnTxt} link={leftBtnLink} />
+            {twoButtons && (
+              <Button imp='secondary' text={rightBtnTxt} link={rightBtnLink} />
+            )}
+          </div>
+          {first && (
+            <div className='item__expand'>
+              <ExpandMoreIcon />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
